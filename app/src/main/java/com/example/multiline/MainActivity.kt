95b9e97fb142b8b3dcbe3b8eb7cf2c393714aae7
Kakaoss <Kubek007.w@wp.pl>
2021-11-17 08:42:18 +0100

    Zadanie skończone,napisy się wyświetlają

--- app/src/main/java/com/example/multiline/MainActivity.kt
+++ app/src/main/java/com/example/multiline/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.multiline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnMultiline).setOnClickListener {
+            findViewById<EditText>(R.id.textMultiline).setText(R.string.hardcode)
+        }
+        findViewById<Button>(R.id.btnLine).setOnClickListener {
+            findViewById<EditText>(R.id.plainText).setText(R.string.hardcode)
+        }
     }
 }
+
